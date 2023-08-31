5af48eeba48c22b8664bdceb67756a2130aaa4ea
gvstang <farih.282828@gmail.com>
2023-08-31 22:16:40 +0700

    add source code bab 02

---
+++ 02/InputOutput.py
@@ -0,0 +1,13 @@
+name = input("Enter your name: ")
+
+print("Hello, " + name)
+print(type(name))
+print(dir(name))
+
+integer = int(input("Enter your int: "))
+print(integer)
+print(type(integer))
+
+pecahan = float(input("Enter your int: "))
+print(pecahan)
+print(type(pecahan))
