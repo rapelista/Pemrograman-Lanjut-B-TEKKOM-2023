bd53b0d7e4689878e235676bcd3db6a5b99c6386
gvstang <farih.282828@gmail.com>
2023-08-31 22:23:11 +0700

    add source code bab 03

---
+++ 03/Angka.py
@@ -0,0 +1,12 @@
+for i in range(1,101,1):
+    print(i, end = ' ')
+
+# for i in 'looping':
+#     if i == 'o':
+#         continue
+#     print('Huruf sekarang : ', i)
+
+# for i in 'looping':
+#     if i == 'o':
+#         break
+#     print('Huruf sekarang : ', i)
